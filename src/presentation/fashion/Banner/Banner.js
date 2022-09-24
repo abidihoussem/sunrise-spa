6a1797b750adb44015a7d6b7fb7593fd4afa5f57
ABIDI Houssem <abidihouss@gmail.com>
2022-09-24 22:47:35 +0200

    slider from contentful veru ugly

--- src/presentation/fashion/Banner/Banner.js
+++ src/presentation/fashion/Banner/Banner.js
@@ -1,26 +1,13 @@
 import { VueperSlides, VueperSlide } from 'vueperslides';
 import 'vueperslides/dist/vueperslides.css';
 import { useI18n } from 'vue-i18n';
+import { createClient} from 'contentful';
+import { ref } from 'vue';
 
 export default {
   setup() {
-    const { t } = useI18n({
-      inheritLocale: true,
-      useScope: 'local',
-    });
-    return {
-      t,
-    };
-  },
-  components: {
-    VueperSlides,
-    VueperSlide,
-  },
-  data: () => ({
-    autoPlaying: true,
-    internalAutoPlaying: true,
-    slides: [
-      {
+    const slides = ref([]);
+    slides.value = [{
         title: 'Slide 1',
         content: {
           bgImage: 'banner1.jpg',
@@ -38,6 +25,56 @@ export default {
           bttnText: 'shopNow',
         },
       },
-    ],
-  }),
+    ];
+
+    const client = createClient({
+      // This is the space ID. A space is like a project folder in Contentful terms
+      space: "w4bajye04bqd",
+      // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+      accessToken: "suMLJ1e_hQNtnMXvVDKsV9D29H1XVV86X6wsxk7eN3Y"
+    });
+    (async () => {
+      const slide1 = await client
+      .getEntry("7YcfFcU4q1JhJzigNchyC");
+      const slide2 = await client
+      .getEntry("111a51m5DRz9XaXpCTHoe1");
+      slides.value = [{
+        title: 'Slide 1',
+        content: {
+          bgImage: slide1.fields.image.fields.file.url,
+          h3Message: 'GET',
+          h1Message: 'INSPIRED',
+          bttnText: 'All Stories',
+        },
+      },
+      {
+        title: 'Slide 2',
+        content: {
+          bgImage: slide2.fields.image.fields.file.url,
+          h3Message: 'Discover more with Canon',
+          h1Message: 'Cameras',
+          bttnText: 'Explore our product range',
+        },
+      },
+    ];
+
+    })();
+
+    const { t } = useI18n({
+      inheritLocale: true,
+      useScope: 'local',
+    });
+    return {
+      t,
+      slides
+    };
+  },
+  components: {
+    VueperSlides,
+    VueperSlide,
+  },
+  data: () => ({
+    autoPlaying: true,
+    internalAutoPlaying: true,
+  })
 };
--- src/presentation/fashion/Banner/Banner.vue
+++ src/presentation/fashion/Banner/Banner.vue
@@ -16,7 +16,7 @@
         <template v-slot:content="">
           <div
             class="single-slider bg-img slider-height-2 align-items-center custom-d-flex"
-            :style="`background-image:url(${require(`presentation/assets/img/${slide.content.bgImage}`)});`"
+            :style="`background-image:url(${slide.content.bgImage}`"
           >
             <div class="container">
               <div
